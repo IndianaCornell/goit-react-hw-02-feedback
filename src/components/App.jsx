322f0659db2086e7bae024318df92f603ff1ac3a
Serj Pyshkin <IndianaCornell@gmail.com>
2023-08-21 19:00:26 +0300

    done
    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,43 +1,66 @@
 import { GlobalStyle } from './GlobalStyle';
 import { Layout } from './Layout';
+import { Card } from './Card.styled';
 
 import { Component } from 'react';
 
+import { Statistics } from './Statistics/Statistics';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Section } from './Sections/Section';
+
 export class App extends Component {
   state = {
-    good: 2,
-    neutral: 8,
-    bad: 5,
+    good: 1,
+    neutral: 0,
+    bad: 0,
+    total: 0,
+    positiveFeedbackPercentage: 4,
+  };
+
+  handleFeedback = option => {
+    this.setState(prevState => {
+      return {
+        [option]: prevState[option] + 1,
+        total: prevState.total + 1,
+      };
+    });
   };
 
   countTotalFeedback() {
-    return this.state.good + this.state.neutral + this.state.bad;
+    return this.state.total;
   }
 
   countPositiveFeedbackPercentage() {
-    const positiveFeedbackPercentage =
-      (this.state.good / this.countTotalFeedback()) * 100;
-    return positiveFeedbackPercentage;
+    return this.state.total === 0
+      ? 0
+      : ((this.state.good / this.state.total) * 100).toFixed(1);
   }
 
   render() {
+    const options = ['good', 'neutral', 'bad'];
     return (
       <Layout>
-        <h1>Please leave feedback</h1>
-        <button>Good</button>
-        <button>Neutral</button>
-        <button>Bad</button>
-        <div>
-          <h2>Statistics</h2>
-          <p>Good: {this.state.good}</p>
-          <p>Neutral: {this.state.neutral}</p>
-          <p>Bad: {this.state.bad}</p>
-          <p>Total: {this.countTotalFeedback()}</p>
-          <p>
-            Positive feedback:{' '}
-            {this.countPositiveFeedbackPercentage().toFixed(1)}%
-          </p>
-        </div>
+        <Card>
+          <Section title="Please leave feedback">
+            <FeedbackOptions
+              clickFeedback={this.handleFeedback}
+              options={options}
+            />
+          </Section>
+          <Section title="Statistics">
+            {this.countTotalFeedback() === 0 ? (
+              <h2>There is no feedback :c</h2>
+            ) : (
+              <Statistics
+                good={this.state.good}
+                neutral={this.state.neutral}
+                bad={this.state.bad}
+                total={this.countTotalFeedback()}
+                positivePercentage={this.countPositiveFeedbackPercentage()}
+              />
+            )}
+          </Section>
+        </Card>
         <GlobalStyle />
       </Layout>
     );
