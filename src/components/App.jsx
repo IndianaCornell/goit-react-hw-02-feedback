31009cf23e2f0f1aa258ae0e62ccb8d5ee29342c
Serj Pyshkin <IndianaCornell@gmail.com>
2023-08-21 17:07:41 +0300

    refactoring
    refactoring

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,45 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { GlobalStyle } from './GlobalStyle';
+import { Layout } from './Layout';
+
+import { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    good: 2,
+    neutral: 8,
+    bad: 5,
+  };
+
+  countTotalFeedback() {
+    return this.state.good + this.state.neutral + this.state.bad;
+  }
+
+  countPositiveFeedbackPercentage() {
+    const positiveFeedbackPercentage =
+      (this.state.good / this.countTotalFeedback()) * 100;
+    return positiveFeedbackPercentage;
+  }
+
+  render() {
+    return (
+      <Layout>
+        <h1>Please leave feedback</h1>
+        <button>Good</button>
+        <button>Neutral</button>
+        <button>Bad</button>
+        <div>
+          <h2>Statistics</h2>
+          <p>Good: {this.state.good}</p>
+          <p>Neutral: {this.state.neutral}</p>
+          <p>Bad: {this.state.bad}</p>
+          <p>Total: {this.countTotalFeedback()}</p>
+          <p>
+            Positive feedback:{' '}
+            {this.countPositiveFeedbackPercentage().toFixed(1)}%
+          </p>
+        </div>
+        <GlobalStyle />
+      </Layout>
+    );
+  }
+}
